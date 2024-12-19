5409f53bbcb2eaf4d6f8a7721b7e047778163ddb
c <c@c-laptop.>
2024-12-19 18:27:15 -0500

    setup tailwind

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+import "../global.css";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl">Home</Text>
+    </View>
+  );
+}
